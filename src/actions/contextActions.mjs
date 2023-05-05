d2aadfd6358f12314d6d64205f60ccfa98413b4f
Stephen Gaito <stephen@perceptisys.co.uk>
2023-05-05 20:00:28 +0100

    refactored loading and parsing of ConTeXt components

--- src/actions/contextActions.mjs
+++ src/actions/contextActions.mjs
@@ -1,7 +1,28 @@
 
-export function registerActions(ScopeActions, Structures) {
+class Components {
 
-  Structures.newStructure('components')
+  pendingComponents = {}
+  loadedComponents  = {}
+
+  pending(aComponent) {
+    this.pendingComponents[aComponent] = true
+  }
+
+  getPending() {
+    return Object.keys(this.pendingComponents)
+  }
+
+  loaded(aComponent) {
+    if (this.pendingComponents[aComponent]) {
+      delete this.pendingComponents[aComponent]
+    }
+    this.loadedComponents[aComponent] = true
+  }
+}
+
+export function registerActions(ScopeActions, Grammars, Structures, config) {
+
+  Structures.newStructure('components', new Components())
 
   ScopeActions.addScopedAction(
     'initialize.control.structure.context',
@@ -17,9 +38,37 @@ export function registerActions(ScopeActions, Structures) {
   )
 
   ScopeActions.addScopedAction(
+    'run.load.components.context',
+    import.meta.url,
+    async function(thisScope, theScope, theTokens) {
+      console.log("----------------------------------------------------------")
+      console.log("runComponent")
+      console.log(`thisScope: ${thisScope}`)
+      console.log(` theScope: ${theScope}`)
+      console.log(`theTokens: ${theTokens}`)
+      console.log("----------------------------------------------------------")
+      const components = Structures.getStructure('components')
+      for (const aDocPath of theTokens) {
+        components.pending(aDocPath)
+      }
+      var someComponents = components.getPending()
+      while (0 < someComponents.length) {
+        console.log(someComponents)
+        for (const aDocPath of someComponents) {
+          components.loaded(aDocPath)
+          await Grammars.traceParseOf(aDocPath, config)
+        }
+        someComponents = components.getPending()
+      }
+    }
+  )
+
+  ScopeActions.addScopedAction(
     'keyword.control.structure.context',
     import.meta.url,
     async function(thisScope, theScope, theTokens) {
+      const components = Structures.getStructure('components')
+      components.pending(theTokens[1]+'.tex')
       console.log("----------------------------------------------------------")
       console.log("loadComponent")
       console.log(`thisScope: ${thisScope}`)
